b21b630a13314ea3004d0447c761584fbf00ae3f
sristy <sristy188269@outlook.com>
2025-11-06 21:10:15 +0530

    added netlfiy.toml and removed log in db file

--- backened_ai/conn.js
+++ backened_ai/conn.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 // const url = "mongodb://localhost:27017/myDatabaseName";
 const url = process.env.MONGO_URI;
 
-console.log("Connecting to db",url)
 mongoose.connect(url)
 .then((res) => {
   console.log(" Database connected successfully");

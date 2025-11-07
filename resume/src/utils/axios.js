9ea1ed426c6e1885a9f393a946145beff7892dd7
sristy <sristy188269@outlook.com>
2025-11-07 08:00:03 +0530

    added backened URL

--- resume/src/utils/axios.js
+++ resume/src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL:"https://resume-analyzer-one-psi.vercel.app/",
 })
 export default instance;
